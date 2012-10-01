0aae50c966f2a65bd83e83b3b38a7330878777ba
"Super" Nathan Weber <nw68868@gmail.com>
2012-10-01 12:40:18 +0900

    Add documentation

--- sample_profile_d.sh
+++ sample_profile_d.sh
@@ -10,6 +10,14 @@
 # line such as:
 # 1:2345:respawn:/bin/login -f root tty1 </dev/tty1 >/dev/tty1 2>&1 # RPICFG_TO_DISABLE
 
+# If you are trying to make this run on boot for a machine that has already been configured,
+# then if you follow all the above instructions you will still be booted into an X session.
+# If you <ctlr>+<alt>+<f1> you will find the raspi-config there ready to run. 
+# In order to boot straight into Raspi-config you must disable lightdm with 
+# $ sudo update-rc.d lightdm disable
+# Then when you reboot the machine, you will be sent straight into the config.
+
+
 if [ $(id -u) -ne 0 ]; then
   printf "\nNOTICE: the software on this Raspberry Pi has not been fully configured. Please run 'sudo raspi-config'\n\n"
 else
